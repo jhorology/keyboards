f79ac2d763bdcf2dbfce6833ce88ae72576e216b
jhorology <jhorology2014@gmail.com>
2022-06-28 06:23:31 +0900

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -8,16 +8,19 @@ KEYMAP="vial"
 
 cd "$(dirname $0)"
 PROJECT=$(pwd)
-SUFFIX="$(date +"%Y%m%d")_applefn_${KEYMAP}"
 mkdir -p dist
 
 cd "$QMK_HOME"
 # checkout to revert changes.
 git checkout .
-qmk setup -H "$QMK_HOME"
 git pull
+QMK_REV=$(git rev-parse --short HEAD)
 make git-submodule
 make clean
+
+VERSION="applefn_${KEYMAP}_$(date +"%Y%m%d")_$(git rev-parse --short HEAD)"
+
+qmk setup -H "$QMK_HOME"
 
 # Apple Fn/Globe key patch
 # see https://gist.github.com/fauxpark/010dcf5d6377c3a71ac98ce37414c6c4
@@ -29,16 +32,16 @@ fi
 cd "${PROJECT}/dist"
 
 qmk compile -j 4 -kb my_keyboards/bakeneko60 -km "$KEYMAP"
-mv "$QMK_HOME/my_keyboards_bakeneko60_$KEYMAP.hex" bakeneko60_${SUFFIX}.hex
+mv "$QMK_HOME/my_keyboards_bakeneko60_$KEYMAP.hex" bakeneko60_${VERSION}.hex
 
 qmk compile -j 4 -kb my_keyboards/ciel60 -km "$KEYMAP"
-mv "$QMK_HOME/my_keyboards_ciel60_$KEYMAP.hex" "ciel60_${SUFFIX}.hex"
+mv "$QMK_HOME/my_keyboards_ciel60_$KEYMAP.hex" ciel60_${VERSION}.hex
 
 qmk compile -j 4 -kb my_keyboards/qk65/solder -km "$KEYMAP"
-mv "$QMK_HOME/my_keyboards_qk65_solder_$KEYMAP.hex" qk65_solder_${SUFFIX}.hex
+mv "$QMK_HOME/my_keyboards_qk65_solder_$KEYMAP.hex" qk65_solder_${VERSION}.hex
 
 qmk compile -j 4 -kb my_keyboards/prime_e/rgb -km "$KEYMAP"
-mv "$QMK_HOME/my_keyboards_prime_e_rgb_$KEYMAP.hex" prime_e_rgb_${SUFFIX}.hex
+mv "$QMK_HOME/my_keyboards_prime_e_rgb_$KEYMAP.hex" prime_e_rgb_${VERSION}.hex
 
 qmk compile -j 4 -kb my_keyboards/dz60rgb_wkl/v2_1 -km "$KEYMAP"
-mv "$QMK_HOME/my_keyboards_dz60rgb_wkl_v2_1_$KEYMAP.bin" dz60rgb_wkl_v2_1_${SUFFIX}.bin
+mv "$QMK_HOME/my_keyboards_dz60rgb_wkl_v2_1_$KEYMAP.bin" dz60rgb_wkl_v2_1_${VERSION}.bin
