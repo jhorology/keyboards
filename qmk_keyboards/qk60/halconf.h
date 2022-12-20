6d0d268efb66d5a15d2e0b8e185b234eb4edd592
jhorology <jhorology2014@gmail.com>
2022-12-21 04:38:31 +0900

    qk60: revert keymaps that was accientally deleted

--- qmk_keyboards/qk60/halconf.h
+++ qmk_keyboards/qk60/halconf.h
@@ -16,10 +16,4 @@
 
 #pragma once
 
-// #define HAL_USE_I2C TRUE
-
-// #define HAL_USE_SPI TRUE
-
-// #define HAL_USE_PWM TRUE
-
 #include_next <halconf.h>
--- qmk_keyboards/qk60/mcuconf.h
+++ qmk_keyboards/qk60/mcuconf.h
@@ -17,12 +17,3 @@
 #pragma once
 
 #include_next <mcuconf.h>
-
-/*
- * I2C driver system settings.
- */
-// #undef STM32_I2C_USE_I2C1
-// #define STM32_I2C_USE_I2C1 TRUE
-
-// #undef STM32_PWM_USE_TIM1
-// #define STM32_PWM_USE_TIM1 TRUE
