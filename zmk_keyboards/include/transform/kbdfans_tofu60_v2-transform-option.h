5756d74bf5fbb190978b4b8adba635a5211d2063
jhorology <jhorology2014@gmail.com>
2023-06-06 17:57:52 +0900

    zmk_keyboards: cleanup

---
+++ zmk_keyboards/include/transform/kbdfans_tofu60_v2-transform-option.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/*
+ * selec option
+ *  0=deselect
+ *  1=select
+ */
+#define SPLIT_BS 1
+#define ISO_ENTER 0
+#define SPLIT_LEFT_SHIFT 0
+#define SPLIT_RIGHT_SHIFT 1
+
+/*
+ * options for bootom row
+ */
+#define BOTTOM_ROW_ANSI 0
+#define BOTTOM_ROW_TSANGAN 1
+#define BOTTOM_ROW_HHKB 2
+#define BOTTOM_ROW_WKL 3
+
+/*
+ * choice of the options for bottom row
+ */
+#define BOTTOM_ROW BOTTOM_ROW_HHKB
---
+++ zmk_keyboards/include/transform/qk60_wired-transform-option.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/*
+ * selec option
+ *  0=deselect
+ *  1=select
+ */
+#define SPLIT_BS 1
+#define SPLIT_RIGHT_SHIFT 1
+#define SPLIT_SPACE 0
+
+/*
+ * options for bootom row
+ */
+#define BOTTOM_ROW_TSANGAN 0
+#define BOTTOM_ROW_HHKB 1
+#define BOTTOM_ROW_WKL 2
+
+/*
+ * choice of the options for bottom row
+ */
+#define BOTTOM_ROW BOTTOM_ROW_HHKB
