1e2821411acad3f32ac9a08792671611594359e2
jhorology <jhorology2014@gmail.com>
2023-07-27 00:32:46 +0900

    zmk_keyboards: improve usb host os detection

--- zmk_keyboards/lib/src/usb_host_os.c
+++ zmk_keyboards/lib/src/usb_host_os.c
@@ -9,16 +9,18 @@ LOG_MODULE_REGISTER(evil, CONFIG_EVIL_LOG_LEVEL);
 
 #define USB_SETUP_LOG_MAX 64
 #define USB_SETUP_TIMEOUT_MS 1000
+#define IS_GET_DEVICE_DESCRIPTOR(setup)                                                                               \
+  (setup->RequestType.recipient == USB_REQTYPE_RECIPIENT_DEVICE &&                                                    \
+   setup->RequestType.type == USB_REQTYPE_TYPE_STANDARD && setup->RequestType.direction == USB_REQTYPE_DIR_TO_HOST && \
+   setup->bRequest == USB_SREQ_GET_DESCRIPTOR)
 
 static usb_host_os_callback user_cb;
-
 static enum usb_host_os detected_os = USB_HOST_OS_UNDEFINED;
 static enum usb_host_os notified_os = USB_HOST_OS_UNDEFINED;
 static volatile int packet_cnt;
-static bool maybe_darwin = false;
-static bool during_setup = false;
+static bool detecting = false;
 static bool end_detect = false;
-
+static uint32_t wlengths = 0;
 static void usb_end_setup(struct k_work *work);
 static K_WORK_DELAYABLE_DEFINE(usb_host_os_work, usb_end_setup);
 
@@ -35,41 +37,49 @@ static inline void notify_os() {
 }
 
 void usb_trace_setup(struct usb_setup_packet *setup) {
-  if (!during_setup) {
-    maybe_darwin = false;
+  bool is_get_device_descriptor = IS_GET_DEVICE_DESCRIPTOR(setup);
+  uint8_t descriptor_type = USB_GET_DESCRIPTOR_TYPE(setup->wValue);
+  if (!detecting && is_get_device_descriptor && descriptor_type == USB_DESC_DEVICE) {
     packet_cnt = 0;
-    during_setup = true;
+    detecting = true;
     end_detect = false;
+    wlengths = 0;
   }
+  if (detecting) {
 #if IS_ENABLED(CONFIG_USB_DETECT_HOST_OS_DEBUG)
-  if (packet_cnt < USB_SETUP_LOG_MAX) {
-    usb_setup_log[packet_cnt] = *setup;
-  }
+    if (packet_cnt < USB_SETUP_LOG_MAX) {
+      usb_setup_log[packet_cnt++] = *setup;
+    }
 #endif  // CONFIG_USB_DETECT_HOST_OS_DEBGU
-  if (!end_detect) {
-    // TODO super easy way for now
-    // TODO dosen't work as expected with nrf52840
-    // macOS send SET_ADDRESS request at first
-    if (packet_cnt == 0 && setup->bmRequestType == 0 && setup->bRequest == USB_SREQ_SET_ADDRESS) {
-      maybe_darwin = true;
+    if (!end_detect) {
+      if (is_get_device_descriptor) {
+        // detect DARWIN (macOS iOS iPadOS)
+        if (descriptor_type == USB_DESC_CONFIGURATION && (wlengths & 0xff00ff00) == 0x02000200) {
+          detected_os = USB_HOST_OS_DARWIN;
+          LOG_DBG("detected os: %d", detected_os);
+          notify_os();
+          end_detect = true;
+        }
+        // TODO other OS
+        wlengths <<= 8;
+        if (descriptor_type == USB_DESC_STRING) {
+          wlengths += setup->wLength & 0xff;
+        }
+      }
     }
-    // igonore packets that are sended when wakeup from sleep
-    if (packet_cnt >= 4 && (setup->RequestType.type != USB_REQTYPE_TYPE_STANDARD ||
-                            setup->RequestType.recipient != USB_REQTYPE_RECIPIENT_DEVICE)) {
-      detected_os = maybe_darwin ? USB_HOST_OS_DARWIN : USB_HOST_OS_UNKNOWN;
-      LOG_DBG("detected os: %d", detected_os);
-      notify_os();
-      end_detect = true;
-    }
+    k_work_reschedule(&usb_host_os_work, K_MSEC(USB_SETUP_TIMEOUT_MS));
   }
-  packet_cnt++;
-  k_work_reschedule(&usb_host_os_work, K_MSEC(USB_SETUP_TIMEOUT_MS));
 }
 
 static void usb_end_setup(struct k_work *work) {
   LOG_DBG("end setup");
-  during_setup = false;
-  notify_os();
+  if (!end_detect) {
+    detected_os = USB_HOST_OS_UNKNOWN;
+    LOG_DBG("detected os: %d", detected_os);
+    notify_os();
+    end_detect = true;
+  }
+  detecting = false;
 }
 
 void enable_usb_host_os(usb_host_os_callback cb) { user_cb = cb; };
