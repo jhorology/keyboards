3e48286e7b49957e27810751e36a8129bf92eee4
jhorology <jhorology2014@gmail.com>
2024-08-10 05:49:46 +0900

    modify 80x128_mono screen

--- zmk_keyboards/lib/zmk/display/80x128_mono/custom_status_screen.c
+++ zmk_keyboards/lib/zmk/display/80x128_mono/custom_status_screen.c
@@ -1,88 +1,93 @@
 /*
+ * Copyright (c) 2020 The ZMK Contributors
  *
- * Copyright (c) 2021 Darryl deHaan
  * SPDX-License-Identifier: MIT
- *
  */
 
-#include <zephyr/logging/log.h>
+#include <zmk/display/widgets/output_status.h>
+#include <zmk/display/widgets/peripheral_status.h>
+#include <zmk/display/widgets/battery_status.h>
+#include <zmk/display/widgets/layer_status.h>
+#include <zmk/display/widgets/wpm_status.h>
 #include <zmk/display/status_screen.h>
 
-#include "widgets/battery_status.h"
-#include "widgets/peripheral_status.h"
-#include "widgets/output_status.h"
-#include "widgets/layer_status.h"
-
+#include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-LV_IMG_DECLARE(layers2);
-
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_BATTERY)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
 static struct zmk_widget_battery_status battery_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_OUTPUT)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
 static struct zmk_widget_output_status output_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_PERIPHERAL)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
 static struct zmk_widget_peripheral_status peripheral_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_LAYER)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
 static struct zmk_widget_layer_status layer_status_widget;
 #endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+static void create_label(lv_obj_t *parent, const char *text, lv_align_t align, lv_coord_t x,
+                         lv_coord_t y) {
+  lv_obj_t *label = lv_label_create(parent);
+  lv_obj_set_style_text_font(label, lv_theme_get_font_small(parent), LV_PART_MAIN);
+  lv_label_set_text(label, text);
+  lv_obj_align(label, align, x, y);
+}
 
 lv_obj_t *zmk_display_status_screen() {
   lv_obj_t *screen;
   screen = lv_obj_create(NULL);
-
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_BATTERY)
-  zmk_widget_battery_status_init(&battery_status_widget, screen);
-  lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_MID, 0, 2);
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_OUTPUT)
-  zmk_widget_output_status_init(&output_status_widget, screen);
-  lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 41);
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_PERIPHERAL)
-  zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
-  lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_MID, 0,
-               41);
-#endif
-
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_LAYER)
-  lv_obj_t *LayersHeading;
-  LayersHeading = lv_img_create(screen);
-  lv_obj_align(LayersHeading, LV_ALIGN_BOTTOM_MID, 0, -30);
-  lv_img_set_src(LayersHeading, &layers2);
+  int y = 0;
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
 
   zmk_widget_layer_status_init(&layer_status_widget, screen);
   lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
-                             &lv_font_montserrat_16, LV_PART_MAIN);
-  lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_MID, 0, -5);
+                             lv_theme_get_font_small(screen), LV_PART_MAIN);
+  lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0, y);
+  y += 18;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_LOGO)
-  lv_obj_t *logo_icon;
-  logo_icon = lv_img_create(screen);
-#  if IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_LOGO_IMAGE_ZEN)
-  LV_IMG_DECLARE(zenlogo);
-  lv_img_set_src(logo_icon, &zenlogo);
-#  elif IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_LOGO_IMAGE_IMAGE_LPKB)
-  LV_IMG_DECLARE(lpkblogo);
-  lv_img_set_src(logo_icon, &lpkblogo);
-#  elif IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_LOGO_IMAGE_ZMK)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+  create_label(screen, "BAT", LV_ALIGN_TOP_LEFT, -1, y + 2);
+  zmk_widget_battery_status_init(&battery_status_widget, screen);
+  lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, y);
+  y += 18;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+  create_label(screen, "OUT", LV_ALIGN_TOP_LEFT, -1, y + 2);
+  zmk_widget_output_status_init(&output_status_widget, screen);
+  lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, 0, y);
+  y += 18;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+  create_label(screen, "LINK", LV_ALIGN_TOP_LEFT, -1, y + 2);
+  zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+  lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_RIGHT, 0,
+               y);
+  y += 18;
+#endif
+
+  // TODO do something fun
+  create_label(screen, "Powerd By", LV_ALIGN_TOP_MID, -1, y + 8);
+  lv_obj_t *logo = lv_img_create(screen);
   LV_IMG_DECLARE(zmklogo);
-  lv_img_set_src(logo_icon, &zmklogo);
-#  elif IS_ENABLED(CONFIG_ZMK_DISPLAY_CUSTOM_WIDGETS_80X128_MONO_LOGO_IMAGE_MIRYOKU)
-  LV_IMG_DECLARE(miryokulogo);
-  lv_img_set_src(logo_icon, &miryokulogo);
-#  endif
-  lv_obj_align(logo_icon, LV_ALIGN_BOTTOM_MID, 0, -5);
-#endif  // CONFIG_CUSTOM_WIDGET_LOGO
+  lv_img_set_src(logo, &zmklogo);
+  lv_obj_align(logo, LV_ALIGN_TOP_MID, 0, y + 24);
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+  zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+  lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+#endif
 
   return screen;
 }
