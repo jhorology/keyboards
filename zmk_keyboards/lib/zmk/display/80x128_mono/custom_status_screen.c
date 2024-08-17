27df55b79eb79b7c171e4f7ae95d14e18caff3d4
jhorology <jhorology2014@gmail.com>
2024-08-17 20:59:36 +0900

    update custom screen for 80x160_mono

--- zmk_keyboards/lib/zmk/display/80x128_mono/custom_status_screen.c
+++ zmk_keyboards/lib/zmk/display/80x128_mono/custom_status_screen.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2020 The ZMK Contributors
- *
  * SPDX-License-Identifier: MIT
  */
 
@@ -14,7 +13,7 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS_80X128_MONO)
 static struct zmk_widget_battery_status battery_status_widget;
 #endif
 
@@ -34,6 +33,10 @@ static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_wpm_status wpm_status_widget;
 #endif
 
+#define MARGIN_BOTTOM 4
+#define MARGIN_LEFT 2
+#define MARGIN_RIGHT 2
+
 static void create_label(lv_obj_t *parent, const char *text, lv_align_t align, lv_coord_t x,
                          lv_coord_t y) {
   lv_obj_t *label = lv_label_create(parent);
@@ -45,44 +48,55 @@ static void create_label(lv_obj_t *parent, const char *text, lv_align_t align, l
 lv_obj_t *zmk_display_status_screen() {
   lv_obj_t *screen;
   screen = lv_obj_create(NULL);
-  int y = 0;
+  int y = MARGIN_BOTTOM / 2;
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
 
   zmk_widget_layer_status_init(&layer_status_widget, screen);
   lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
                              lv_theme_get_font_small(screen), LV_PART_MAIN);
-  lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0, y);
-  y += 18;
+  lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, MARGIN_LEFT,
+               y);
+  y += 14 + MARGIN_BOTTOM;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
-  create_label(screen, "BAT", LV_ALIGN_TOP_LEFT, -1, y + 2);
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS_80X128_MONO)
+  create_label(screen, "BAT", LV_ALIGN_TOP_LEFT, MARGIN_LEFT, y + 2);
   zmk_widget_battery_status_init(&battery_status_widget, screen);
-  lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, y);
-  y += 18;
+  lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT,
+               -MARGIN_RIGHT, y);
+  y += 16 + MARGIN_BOTTOM;
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
-  create_label(screen, "OUT", LV_ALIGN_TOP_LEFT, -1, y + 2);
+  create_label(screen, "OUT", LV_ALIGN_TOP_LEFT, MARGIN_LEFT, y + 2);
   zmk_widget_output_status_init(&output_status_widget, screen);
-  lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, 0, y);
-  y += 18;
+  lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT,
+               -MARGIN_RIGHT, y);
+  y += 16 + MARGIN_BOTTOM;
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
-  create_label(screen, "LINK", LV_ALIGN_TOP_LEFT, -1, y + 2);
+  create_label(screen, "LINK", LV_ALIGN_TOP_LEFT, MARGIN_LEFT, y + 2);
   zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
-  lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_RIGHT, 0,
-               y);
-  y += 18;
+  lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_RIGHT,
+               -MARGIN_RIGHT, y);
+  y += 16 + MARGIN_BOTTOM;
 #endif
 
   // TODO do something fun
-  create_label(screen, "Powerd By", LV_ALIGN_TOP_MID, -1, y + 8);
-  lv_obj_t *logo = lv_img_create(screen);
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+  create_label(screen, "Powerd By", LV_ALIGN_BOTTOM_MID, 0, -40 - MARGIN_BOTTOM);
+  lv_obj_t *zmk = lv_img_create(screen);
   LV_IMG_DECLARE(zmklogo);
-  lv_img_set_src(logo, &zmklogo);
-  lv_obj_align(logo, LV_ALIGN_TOP_MID, 0, y + 24);
+  lv_img_set_src(zmk, &zmklogo);
+  lv_obj_align(zmk, LV_ALIGN_BOTTOM_MID, 0, -MARGIN_BOTTOM);
+#endif
+#if IS_ENABLED(CONFIG_ZMK_SPLIT) && !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+  lv_obj_t *starman = lv_img_create(screen);
+  LV_IMG_DECLARE(starman_80x80);
+  lv_img_set_src(starman, &starman_80x80);
+  lv_obj_align(starman, LV_ALIGN_BOTTOM_MID, 0, 0);
+#endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
   zmk_widget_wpm_status_init(&wpm_status_widget, screen);
