64bc3211b699b80694374551ec7ee7096e367a64
jhorology <jhorology2014@gmail.com>
2024-09-11 18:48:57 +0900

    qmk b60: update keymap

--- qmk_keyboards/dropbear60_solder/keymaps/default/keymap.c
+++ qmk_keyboards/dropbear60_solder/keymaps/default/keymap.c
@@ -19,11 +19,12 @@
 // tap dance
 // [single tap, single hold, multi tap, tap hold, tapping term]
 const tap_dance_entry_t PROGMEM tap_dance_predefined_entries[NUM_TAP_DANCE_PREDEFINED_ENTRIES] = {
-  // TD(0) - Apple Fn key + IME switch
-  {KC_LNG2, APPLE_FF, KC_LNG1, APPLE_FF, TAPPING_TERM},
-  // TD(1) - Protect layer 3 from misstouch, MENU + MO(3)
-  {KC_APP, KC_APP, KC_APP, MO(3), TAPPING_TERM},
-};
+  // TD(0) - LCMD + Spacemacs leader key
+  [0] = {A(KC_M), KC_LGUI, KC_LGUI, KC_LGUI, TAPPING_TERM},
+  // TD(1) - RCMD + LANG2/LANG1
+  [1] = {KC_LNG2, KC_RGUI, KC_LNG1, KC_RGUI, TAPPING_TERM},
+  // TD(2) - Protect layer 3 from misstouch, MENU + MO(3)
+  [2] = {KC_APP, KC_APP, KC_APP, MO(3), TAPPING_TERM}};
 
 // since QMK 0.26, tap_dance_actions must be defined in keyma.c
 tap_dance_action_t tap_dance_actions[TAP_DANCE_ENTRIES];
@@ -32,19 +33,19 @@ tap_dance_action_t tap_dance_actions[TAP_DANCE_ENTRIES];
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // mac base layer
   [0] = LAYOUT_60_tsangan_hhkb(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(2),
-    TD(0),   KC_LALT, KC_LGUI,                            KC_SPC,                                               KC_RGUI, KC_RALT, TD(1)
+    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC,
+    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
+    KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(2),
+    APPLE_FF, KC_LALT, TD(0),                              KC_SPC,                                               TD(1),   KC_RALT, TD(2)
   ),
   // standard base layer
   [1] = LAYOUT_60_tsangan_hhkb(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(2),
-    TD(0),   KC_LALT, KC_LGUI,                            KC_SPC,                                               KC_RGUI, KC_RALT, TD(1)
+    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC,
+    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
+    KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(2),
+    APPLE_FF, KC_LALT, TD(0),                              KC_SPC,                                               TD(1),   KC_RALT, TD(2)
   ),
   // HHKB-like fn layer
   // + shift key for RGB decrement functions
