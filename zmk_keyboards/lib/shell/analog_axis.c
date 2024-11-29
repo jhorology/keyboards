56f7359d09e281f721a61cebe5fe4a210556cf4e
jhorology <jhorology2014@gmail.com>
2024-11-30 04:30:10 +0900

    update analog_axis shell comannd

--- zmk_keyboards/lib/shell/analog_axis.c
+++ zmk_keyboards/lib/shell/analog_axis.c
@@ -7,16 +7,51 @@
 #include <zephyr/input/input_analog_axis_settings.h>
 #include <zephyr/input/input_analog_axis.h>
 
-static struct analog_axis_calibration cal_data;
-static bool cal_start;
+#include <util/qsort.h>
+
+#define SAMPLE_BUF_SIZE 63
+
+struct sample_result {
+  int16_t min;
+  int16_t median;
+  int16_t max;
+};
+
+static int16_t sample_buf[SAMPLE_BUF_SIZE];
+static volatile size_t sample_cnt;
 static int cal_ch = -1;
-static int16_t raw_val;
-static int16_t cal_min;
-static int16_t cal_max;
+
+static int16_t raw_val[CONFIG_INPUT_ANALOG_AXIS_SETTINGS_MAX_AXES];
 static int32_t out_x;
 static int32_t out_y;
 static int32_t out_z;
 static const struct device *const dev = DEVICE_DT_GET_ANY(analog_axis);
+
+static int confirm(const struct shell *sh) {
+  char c;
+  size_t cnt;
+  while (true) {
+    sh->iface->api->read(sh->iface, &c, sizeof(c), &cnt);
+    if (c == 'n' || c == 'N') {
+      return -1;
+    }
+    if (c == 'y' || c == 'Y') {
+      return 0;
+    }
+    k_msleep(300);
+  }
+  return 0;
+}
+
+static int stop(const struct shell *sh) {
+  char c;
+  size_t cnt;
+  sh->iface->api->read(sh->iface, &c, sizeof(c), &cnt);
+  if (c == 's' || c == 'S') {
+    return -1;
+  }
+  return 0;
+}
 
 static void analog_axis_event_cb(struct input_event *evt) {
   switch (evt->code) {
@@ -34,142 +69,186 @@ static void analog_axis_event_cb(struct input_event *evt) {
 INPUT_CALLBACK_DEFINE(dev, analog_axis_event_cb);
 
 static void analog_axis_raw_data_cb(const struct device *dev, int ch, int16_t raw) {
-  if (ch == cal_ch) {
-    raw_val = raw;
-    if (cal_start || raw_val < cal_min) {
-      cal_min = raw_val;
-    }
-    if (cal_start || raw_val > cal_max) {
-      cal_max = raw_val;
-    }
+  if (ch >= 0 && ch < CONFIG_INPUT_ANALOG_AXIS_SETTINGS_MAX_AXES) {
+    raw_val[ch] = raw;
   }
-  cal_start = false;
+  if (ch == cal_ch && sample_cnt < SAMPLE_BUF_SIZE) {
+    sample_buf[sample_cnt++] = raw;
+  }
+}
+
+static struct sample_result get_sample_result(int ch) {
+  struct sample_result res;
+  int16_t _tmp;
+#define LESS(i, j) sample_buf[i] < sample_buf[j]
+#define SWAP(i, j) _tmp = sample_buf[i], sample_buf[i] = sample_buf[j], sample_buf[j] = _tmp
+  analog_axis_set_raw_data_cb(dev, &analog_axis_raw_data_cb);
+  cal_ch = ch;
+  sample_cnt = 0;
+
+  while (sample_cnt < SAMPLE_BUF_SIZE) k_msleep(100);
+
+  QSORT(SAMPLE_BUF_SIZE, LESS, SWAP);
+
+  cal_ch = -1;
+  analog_axis_set_raw_data_cb(dev, NULL);
+  res.min = sample_buf[0];
+  res.median = sample_buf[SAMPLE_BUF_SIZE / 2];
+  res.max = sample_buf[SAMPLE_BUF_SIZE - 1];
+  return res;
 }
 
 static int check_device(const struct shell *sh) {
   if (dev == NULL) {
-    shell_print(sh, "Error: Could not find device\n");
+    shell_error(sh, "Error: Could not find device\n");
     return -1;
   }
 
   if (!device_is_ready(dev)) {
-    shell_print(sh, "Error: Device is not ready.\n");
-    return -1;
+    shell_error(sh, "Error: Device is not ready.\n");
+    return -2;
   }
   return 0;
 }
-static int cmd_show(const struct shell *sh, size_t argc, char **argv) {
-  int err = check_device(sh);
-  if (err < 0) {
-    return err;
-  }
 
-  int num_axes = analog_axis_num_axes(dev);
+static int cmd_show(const struct shell *sh, size_t argc, char **argv) {
+  static struct analog_axis_calibration cal_data;
+  int num_axes;
+
+  if (check_device(sh)) return -1;
+
+  num_axes = analog_axis_num_axes(dev);
   shell_print(sh, "Current settings:");
   for (int ch = 0; ch < num_axes; ch++) {
-    struct analog_axis_calibration *cal = &cal_data;
-    analog_axis_calibration_get(dev, ch, cal);
-    shell_print(sh, "ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, (int)cal->in_min,
-                (int)cal->in_max, (int)cal->in_deadzone);
+    analog_axis_calibration_get(dev, ch, &cal_data);
+    shell_info(sh, "ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, cal_data.in_min,
+               cal_data.in_max, cal_data.in_deadzone);
   }
-
   return 0;
 }
 
 static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
-  int err = check_device(sh);
-  if (err < 0) {
-    return err;
-  }
-  int num_axes = analog_axis_num_axes(dev);
-  int ch = atoi(argv[1]);
+#define CAL_ZERO_NUM_TIMES 10
+  struct analog_axis_calibration cal_data;
+  struct sample_result res;
+  int ch;
+  int16_t out_min;
+  int16_t out_max;
+  int num_axes;
+  int sum = 0;
+  int min;
+  int max;
+  int origin;
+  int16_t deadzone;
+  int pos_noise;
+  int neg_noise;
+  int pos_range;
+  int neg_range;
+
+  if (check_device(sh)) return -1;
+
+  ch = atoi(argv[1]);
+  /* TODO get from DT  */
+  out_min = atoi(argv[2]);
+  out_max = atoi(argv[3]);
+
+  num_axes = analog_axis_num_axes(dev);
   if (ch >= num_axes || ch < 0) {
-    shell_print(sh, "Error: Invalid <ch> argument\n");
+    shell_error(sh, "Error: Invalid <ch> argument\n");
     return -1;
   }
 
-  struct analog_axis_calibration *cal = &cal_data;
-  analog_axis_calibration_get(dev, ch, cal);
-
-  cal_ch = ch;
-  cal_start = true;
-  analog_axis_set_raw_data_cb(dev, &analog_axis_raw_data_cb);
-  out_x = 0;
-  out_y = 0;
-  out_z = 0;
-  char c;
-  size_t cnt;
-
-  while (true) {
-    shell_info(sh, "ch.%d raw value=%d, min=%d, max=%d, out=(x:%d, y:%d, z:%d)", ch, raw_val,
-               cal_min, cal_max, out_x, out_y, out_z);
-
-    sh->iface->api->read(sh->iface, &c, sizeof(c), &cnt);
-    if (c == 'Q' || c == 'q') {
-      break;
-    }
-    if (c == 'S' || c == 's') {
-      shell_print(sh, "\nOld setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
-                  (int)cal->in_min, (int)cal->in_max, (int)cal->in_deadzone);
-      cal->in_max = cal_max;
-      cal->in_min = cal_min;
-      analog_axis_calibration_set(dev, ch, cal);
-      analog_axis_calibration_save(dev);
-
-      analog_axis_calibration_get(dev, ch, cal);
-      shell_print(sh, "New setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
-                  (int)cal->in_min, (int)cal->in_max, (int)cal->in_deadzone);
-      break;
-    }
-
-    k_msleep(500);
+  if (out_min >= out_max) {
+    shell_error(sh, "Error: Invalid <out_min>, <out_max> arguments\n");
+    return -1;
   }
-  analog_axis_set_raw_data_cb(dev, NULL);
+
+  shell_print(
+    sh,
+    "\nSet the ch.%d axis to zero(or origin) position and then don't touch while calibrating.\n"
+    "Are you sure to start? [y/n]",
+    ch);
+  if (confirm(sh)) return -1;
+
+  for (int i = 0; i < CAL_ZERO_NUM_TIMES; i++) {
+    res = get_sample_result(ch);
+    if (i == 0 || res.min < min) min = res.min;
+    if (i == 0 || res.max > max) max = res.max;
+    sum += res.median;
+    origin = sum / (i + 1);
+    shell_print(sh, "%d/%d  min:%d, median:%d, max:%d -> min:%d, average:%d, max:%d", i + 1,
+                CAL_ZERO_NUM_TIMES, res.min, res.median, res.max, min, origin, max);
+  }
+  deadzone = (max - min) * 3 / 2;  // moise width x 1.5
+  pos_noise = max - origin;
+  neg_noise = origin - min;
+
+  shell_print(sh,
+              "Done.\n\nNext, move the ch.%d axis from end to end several times.\n"
+              "Are you sure to start? [y/n]",
+              ch);
+  if (confirm(sh)) return -1;
+
+  min = origin, max = origin;
+  while (true) {
+    res = get_sample_result(ch);
+    if (res.min < min) min = res.min;
+    if (res.max > max) max = res.max;
+    shell_info(sh, "min:%d, max:%d\tPress [S] to stop", min, max);
+    if (stop(sh)) break;
+  }
+  min += pos_noise;
+  max -= neg_noise;
+
+  // adjust zero position
+  if (out_min < 0 && out_max > 0) {
+    pos_range = max - origin;
+    neg_range = origin - min;
+
+    if (pos_range * abs(out_min) > neg_range * out_max) {
+      // offseted positive
+      max = origin + neg_range * out_max / abs(out_min);
+    } else {
+      // offseted negative
+      min = origin - pos_range * abs(out_min) / out_max;
+    }
+  }
+
+  analog_axis_calibration_get(dev, ch, &cal_data);
+  shell_info(sh, "Done.\n\nOld setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
+             cal_data.in_min, cal_data.in_max, cal_data.in_deadzone);
+  shell_info(sh, "Calibrated setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, min, max,
+             deadzone);
+
+  shell_print(sh, "\nAre you sure to save new settting? [y/n]");
+  if (confirm(sh)) return -1;
+  cal_data.in_min = min;
+  cal_data.in_max = max;
+  cal_data.in_deadzone = deadzone;
+  analog_axis_calibration_set(dev, ch, &cal_data);
+  analog_axis_calibration_save(dev);
 
   return 0;
 }
 
-static int cmd_center(const struct shell *sh, size_t argc, char **argv) {
-  int err = check_device(sh);
-  if (err < 0) {
-    return err;
+static int cmd_test(const struct shell *sh, size_t argc, char **argv) {
+  if (check_device(sh)) return -1;
+
+  analog_axis_set_raw_data_cb(dev, &analog_axis_raw_data_cb);
+  while (true) {
+    shell_print(sh, "[%d, %d, %d]\t->\tx:%d, y:%d, z:%d.\tPress [S] to stop.", raw_val[0],
+                raw_val[1], raw_val[2], out_x, out_y, out_z);
+    if (stop(sh)) break;
+    k_msleep(300);
   }
-
-  int num_axes = analog_axis_num_axes(dev);
-  int ch = atoi(argv[1]);
-  if (ch >= num_axes || ch < 0) {
-    shell_print(sh, "Error: Invalid <ch> argument\n");
-    return -1;
-  }
-  struct analog_axis_calibration *cal = &cal_data;
-  analog_axis_calibration_get(dev, ch, cal);
-  shell_print(sh, "Old setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
-              (int)cal->in_min, (int)cal->in_max, (int)cal->in_deadzone);
-
-  int center = atoi(argv[2]);
-  int d_max = cal->in_max - center;
-  int d_min = center - cal->in_min;
-
-  if (d_max > d_min) {
-    cal->in_max = center + d_min;
-  } else {
-    cal->in_min = center - d_max;
-  }
-  analog_axis_calibration_set(dev, ch, cal);
-  analog_axis_calibration_save(dev);
-
-  analog_axis_calibration_get(dev, ch, cal);
-  shell_print(sh, "New setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
-              (int)cal->in_min, (int)cal->in_max, (int)cal->in_deadzone);
-
+  analog_axis_set_raw_data_cb(dev, NULL);
   return 0;
 }
 
 static int cmd_deadzone(const struct shell *sh, size_t argc, char **argv) {
-  int err = check_device(sh);
-  if (err < 0) {
-    return err;
-  }
+  struct analog_axis_calibration cal_data;
+
+  if (check_device(sh)) return -1;
 
   int num_axes = analog_axis_num_axes(dev);
   int ch = atoi(argv[1]);
@@ -177,32 +256,29 @@ static int cmd_deadzone(const struct shell *sh, size_t argc, char **argv) {
     shell_print(sh, "Error: Invalid <ch> argument\n");
     return -1;
   }
-  struct analog_axis_calibration *cal = &cal_data;
-  analog_axis_calibration_get(dev, ch, cal);
-  shell_print(sh, "Old setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
-              (int)cal->in_min, (int)cal->in_max, (int)cal->in_deadzone);
 
-  cal->in_deadzone = atoi(argv[2]);
+  analog_axis_calibration_get(dev, ch, &cal_data);
+  shell_print(sh, "Old setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, cal_data.in_min,
+              cal_data.in_max, cal_data.in_deadzone);
 
-  analog_axis_calibration_set(dev, ch, cal);
+  cal_data.in_deadzone = atoi(argv[2]);
+
+  analog_axis_calibration_set(dev, ch, &cal_data);
   analog_axis_calibration_save(dev);
 
-  analog_axis_calibration_get(dev, ch, cal);
-  shell_print(sh, "New setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
-              (int)cal->in_min, (int)cal->in_max, (int)cal->in_deadzone);
+  analog_axis_calibration_get(dev, ch, &cal_data);
+  shell_print(sh, "New setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, cal_data.in_min,
+              cal_data.in_max, cal_data.in_deadzone);
 
   return 0;
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(
-  analog_axis_sub_cmds, SHELL_CMD_ARG(show, NULL, "show currnt calibration data", cmd_show, 1, 0),
-  SHELL_CMD_ARG(cal, NULL,
-                "usage: analog_axis cal <ch>  |  calibrate in_min and in_max, [qQ] to quit without "
-                "save, [sS] to quit and save data",
-                cmd_cal, 2, 0),
-  SHELL_CMD_ARG(center, NULL, "usage: analog_axis center <ch> <center value>  |  adjust center",
-                cmd_center, 3, 0),
-  SHELL_CMD_ARG(deadzone, NULL, "usage: analog_axis deadzone <ch> <value>  |  set deadzone",
+  analog_axis_sub_cmds, SHELL_CMD_ARG(show, NULL, "Show current calibration data", cmd_show, 1, 0),
+  SHELL_CMD_ARG(cal, NULL, "Calibration\tUsage: analog_axis cal <ch> <out_min> <out_max>", cmd_cal,
+                4, 0),
+  SHELL_CMD_ARG(test, NULL, "Test analog_axis\tUsage: analog_axis test, cmd_test", cmd_test, 1, 0),
+  SHELL_CMD_ARG(deadzone, NULL, "Set deadzone value\tUsage: analog_axis deadzone <ch> <value>",
                 cmd_deadzone, 3, 0),
 
   SHELL_SUBCMD_SET_END /* Array terminated. */
