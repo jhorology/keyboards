9320bc9c57a35dfc191f8f771ba2026c9803fe2b
jhorology <jhorology2014@gmail.com>
2024-12-21 14:30:20 +0900

    improve analog_axis calibration

--- zmk_keyboards/lib/shell/analog_axis.c
+++ zmk_keyboards/lib/shell/analog_axis.c
@@ -6,6 +6,8 @@
 #include <zephyr/input/input.h>
 #include <zephyr/input/input_analog_axis_settings.h>
 #include <zephyr/input/input_analog_axis.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/util_macro.h>
 
 #define SAMPLE_BUF_SIZE 63
 
@@ -15,15 +17,57 @@ struct sample_result {
   int16_t max;
 };
 
+struct channel_cfg {
+  bool invert_output;
+  int16_t out_min;
+  int16_t out_max;
+  uint16_t axis;
+};
+
+struct channel_data {
+  int16_t raw_val;
+  int32_t out_val;
+};
+
+struct channel {
+  const struct channel_cfg cfg;
+  struct channel_data data;
+};
+
+struct device_data {
+  const struct device *dev;
+  size_t num_channels;
+  struct channel_data ch_data[CONFIG_INPUT_ANALOG_AXIS_SETTINGS_MAX_AXES];
+  struct channel_cfg ch_cfg[CONFIG_INPUT_ANALOG_AXIS_SETTINGS_MAX_AXES];
+};
+
+#define _NODE_SEP(node) node,
+#define _NODE(node) node
+#define _NUM_LIST(...) UTIL_INC(NUM_VA_ARGS_LESS_1(__VA_ARGS__))
+
+#define ANALOG_AXIS_NODE_LIST LIST_DROP_EMPTY(DT_FOREACH_STATUS_OKAY(analog_axis, _NODE_SEP))
+#define ANALOG_AXIS_CH_LIST(node) DT_FOREACH_CHILD_STATUS_OKAY_SEP(node, _NODE, (, ))
+
+#define NUM_DEVICES _NUM_LIST(_ANALOG_AXIS_NODE_LIST)
+
+#define ANALOG_AXIS_CH_CFG(node)                  \
+  {.invert_output = DT_PROP(node, invert_output), \
+   .out_min = (int16_t)DT_PROP(node, out_min),    \
+   .out_max = (int16_t)DT_PROP(node, out_max),    \
+   .axis = DT_PROP(node, zephyr_axis)}
+
+#define ANALOG_AXIS_DEVICE(node)                         \
+  {.dev = DEVICE_DT_GET(node),                           \
+   .num_channels = _NUM_LIST(ANALOG_AXIS_CH_LIST(node)), \
+   .ch_cfg = {DT_FOREACH_CHILD_SEP(node, ANALOG_AXIS_CH_CFG, (, ))}},
+
+static struct device_data devs[NUM_DEVICES] = {
+  DT_FOREACH_STATUS_OKAY(analog_axis, ANALOG_AXIS_DEVICE)};
+
 static int16_t sample_buf[SAMPLE_BUF_SIZE];
 static volatile size_t sample_cnt;
+static int cal_dev = -1;
 static int cal_ch = -1;
-
-static int16_t raw_val[CONFIG_INPUT_ANALOG_AXIS_SETTINGS_MAX_AXES];
-static int32_t out_x;
-static int32_t out_y;
-static int32_t out_z;
-static const struct device *const dev = DEVICE_DT_GET_ANY(analog_axis);
 
 static int confirm(const struct shell *sh) {
   char c;
@@ -51,26 +95,39 @@ static int stop(const struct shell *sh) {
   return 0;
 }
 
-static void analog_axis_event_cb(struct input_event *evt) {
-  switch (evt->code) {
-    case INPUT_ABS_X:
-      out_x = evt->value;
-      break;
-    case INPUT_ABS_Y:
-      out_y = evt->value;
-      break;
-    case INPUT_ABS_Z:
-      out_z = evt->value;
-      break;
+static void analog_axis_listener(const struct device *dev, int dev_index, struct input_event *evt) {
+  struct device_data *data = &devs[dev_index];
+  for (size_t i = 0; i < data->num_channels; i++) {
+    if (evt->code == data->ch_cfg[i].axis) {
+      data->ch_data[i].out_val = evt->value;
+      return;
+    }
   }
 }
-INPUT_CALLBACK_DEFINE(dev, analog_axis_event_cb);
 
 static void analog_axis_raw_data_cb(const struct device *dev, int ch, int16_t raw) {
-  if (ch >= 0 && ch < CONFIG_INPUT_ANALOG_AXIS_SETTINGS_MAX_AXES) {
-    raw_val[ch] = raw;
+  struct device_data *data;
+  int dev_index = -1;
+  for (size_t i = 0; i < NUM_DEVICES; i++) {
+    if (dev == devs[i].dev) {
+      dev_index = i;
+      data = &devs[i];
+      break;
+    }
   }
-  if (ch == cal_ch && sample_cnt < SAMPLE_BUF_SIZE) {
+  if (dev_index == -1) {
+    return;
+  }
+
+  if (ch >= 0 && ch < data->num_channels) {
+    data->ch_data[ch].raw_val = raw;
+  }
+
+  if (cal_dev < 0 || cal_ch < 0) {
+    return;
+  }
+
+  if (dev_index == cal_dev && ch == cal_ch && sample_cnt < SAMPLE_BUF_SIZE) {
     sample_buf[sample_cnt++] = raw;
   }
 }
@@ -82,17 +139,21 @@ static int compare_sample(const void *a, const void *b) {
   return (aa > bb) - (aa < bb);
 };
 
-static struct sample_result get_sample_result(int ch) {
+static struct sample_result get_sample_result(int dev_index, int ch) {
   struct sample_result res;
 
-  analog_axis_set_raw_data_cb(dev, &analog_axis_raw_data_cb);
+  // start
   cal_ch = ch;
+  cal_dev = dev_index;
   sample_cnt = 0;
+  analog_axis_set_raw_data_cb(devs[dev_index].dev, &analog_axis_raw_data_cb);
 
   while (sample_cnt < SAMPLE_BUF_SIZE) k_msleep(100);
 
+  // stop
+  analog_axis_set_raw_data_cb(devs[dev_index].dev, NULL);
   cal_ch = -1;
-  analog_axis_set_raw_data_cb(dev, NULL);
+  cal_dev = -1;
 
   qsort(&sample_buf[0], SAMPLE_BUF_SIZE, 2, &compare_sample);
 
@@ -102,14 +163,13 @@ static struct sample_result get_sample_result(int ch) {
   return res;
 }
 
-static int check_device(const struct shell *sh) {
-  if (dev == NULL) {
-    shell_error(sh, "Error: Could not find device\n");
+static int check_device(const struct shell *sh, size_t dev_index) {
+  if (dev_index < 0 || dev_index >= NUM_DEVICES) {
+    shell_error(sh, "Error: Invalid device index\n");
     return -1;
   }
-
-  if (!device_is_ready(dev)) {
-    shell_error(sh, "Error: Device is not ready.\n");
+  if (!device_is_ready(devs[dev_index].dev)) {
+    shell_error(sh, "Error: dev.%d is not ready.\n", dev_index);
     return -2;
   }
   return 0;
@@ -117,28 +177,34 @@ static int check_device(const struct shell *sh) {
 
 static int cmd_show(const struct shell *sh, size_t argc, char **argv) {
   static struct analog_axis_calibration cal_data;
-  int num_axes;
 
-  if (check_device(sh)) return -1;
+  for (size_t i = 0; i < NUM_DEVICES; i++) {
+    if (check_device(sh, i)) return -1;
 
-  num_axes = analog_axis_num_axes(dev);
-  shell_print(sh, "Current settings:");
-  for (int ch = 0; ch < num_axes; ch++) {
-    analog_axis_calibration_get(dev, ch, &cal_data);
-    shell_info(sh, "ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, cal_data.in_min,
-               cal_data.in_max, cal_data.in_deadzone);
+    shell_print(sh, "dev.%d (%s) settings:", i, devs[i].dev->name);
+    for (size_t ch = 0; ch < devs[i].num_channels; ch++) {
+      const struct channel_cfg *cfg = &devs[i].ch_cfg[ch];
+      analog_axis_calibration_get(devs[i].dev, ch, &cal_data);
+      shell_info(sh,
+                 "\tch.%d in_min: %d, in_max: %d, in_deadzone: %d, out_min: %d, out_max: %d, "
+                 "invert_output: %s",
+                 ch, cal_data.in_min, cal_data.in_max, cal_data.in_deadzone, cfg->out_min,
+                 cfg->out_max, cfg->invert_output ? "true" : "false");
+    }
   }
   return 0;
 }
 
 static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
 #define CAL_ZERO_NUM_TIMES 10
+  struct device_data *data;
   struct analog_axis_calibration cal_data;
   struct sample_result res;
+  int dev_index;
   int ch;
   int16_t out_min;
   int16_t out_max;
-  int num_axes;
+  bool invert_output;
   int sum = 0;
   int min;
   int max;
@@ -149,21 +215,22 @@ static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
   int pos_range;
   int neg_range;
 
-  if (check_device(sh)) return -1;
+  dev_index = atoi(argv[1]);
+  if (check_device(sh, dev_index)) return -1;
+  data = &devs[dev_index];
 
-  ch = atoi(argv[1]);
-  /* TODO get from DT  */
-  out_min = atoi(argv[2]);
-  out_max = atoi(argv[3]);
-
-  num_axes = analog_axis_num_axes(dev);
-  if (ch >= num_axes || ch < 0) {
+  ch = atoi(argv[2]);
+  if (ch >= data->num_channels || ch < 0) {
     shell_error(sh, "Error: Invalid <ch> argument\n");
     return -1;
   }
 
+  out_min = data->ch_cfg[ch].out_min;
+  out_max = data->ch_cfg[ch].out_max;
+  invert_output = data->ch_cfg[ch].invert_output;
+
   if (out_min >= out_max) {
-    shell_error(sh, "Error: Invalid <out_min>, <out_max> arguments\n");
+    shell_error(sh, "Error: Invalid <out_min>, <out_max> properties\n");
     return -1;
   }
 
@@ -175,7 +242,7 @@ static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
   if (confirm(sh)) return -1;
 
   for (int i = 0; i < CAL_ZERO_NUM_TIMES; i++) {
-    res = get_sample_result(ch);
+    res = get_sample_result(dev_index, ch);
     if (i == 0 || res.min < min) min = res.min;
     if (i == 0 || res.max > max) max = res.max;
     sum += res.median;
@@ -183,9 +250,9 @@ static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
     shell_print(sh, "%d/%d  min:%d, median:%d, max:%d -> min:%d, average:%d, max:%d", i + 1,
                 CAL_ZERO_NUM_TIMES, res.min, res.median, res.max, min, origin, max);
   }
-  deadzone = (max - min) * 3 / 2;  // moise width x 1.5
   pos_noise = max - origin;
   neg_noise = origin - min;
+  deadzone = MAX(pos_noise, neg_noise) * 3;  // noise width x 1.5
 
   shell_print(sh,
               "Done.\n\nNext, move the ch.%d axis from end to end several times.\n"
@@ -195,7 +262,7 @@ static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
 
   min = origin, max = origin;
   while (true) {
-    res = get_sample_result(ch);
+    res = get_sample_result(dev_index, ch);
     if (res.min < min) min = res.min;
     if (res.max > max) max = res.max;
     shell_info(sh, "min:%d, max:%d\tPress [S] to stop", min, max);
@@ -206,8 +273,13 @@ static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
 
   // adjust zero position
   if (out_min < 0 && out_max > 0) {
-    pos_range = max - origin;
-    neg_range = origin - min;
+    if (invert_output) {
+      pos_range = origin - min;
+      neg_range = max - origin;
+    } else {
+      pos_range = max - origin;
+      neg_range = origin - min;
+    }
 
     if (pos_range * abs(out_min) > neg_range * out_max) {
       // offseted positive
@@ -218,72 +290,102 @@ static int cmd_cal(const struct shell *sh, size_t argc, char **argv) {
     }
   }
 
-  analog_axis_calibration_get(dev, ch, &cal_data);
-  shell_info(sh, "Done.\n\nOld setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch,
-             cal_data.in_min, cal_data.in_max, cal_data.in_deadzone);
-  shell_info(sh, "Calibrated setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, min, max,
-             deadzone);
+  analog_axis_calibration_get(data->dev, ch, &cal_data);
+  shell_info(sh, "Done.\n\nOld setting: dev.%d ch.%d in_min: %d, in_max: %d, in_deadzone: %d",
+             dev_index, ch, cal_data.in_min, cal_data.in_max, cal_data.in_deadzone);
+  shell_info(sh, "Calibrated setting: dev.%d ch.%d in_min: %d, in_max: %d, in_deadzone: %d",
+             dev_index, ch, min, max, deadzone);
 
   shell_print(sh, "\nAre you sure to save new settting? [y/n]");
   if (confirm(sh)) return -1;
   cal_data.in_min = min;
   cal_data.in_max = max;
   cal_data.in_deadzone = deadzone;
-  analog_axis_calibration_set(dev, ch, &cal_data);
-  analog_axis_calibration_save(dev);
+  analog_axis_calibration_set(data->dev, ch, &cal_data);
+  analog_axis_calibration_save(data->dev);
 
   return 0;
 }
 
 static int cmd_test(const struct shell *sh, size_t argc, char **argv) {
-  if (check_device(sh)) return -1;
+  struct device_data *data;
+  int dev_index;
 
-  analog_axis_set_raw_data_cb(dev, &analog_axis_raw_data_cb);
+  dev_index = atoi(argv[1]);
+  if (check_device(sh, dev_index)) return -1;
+  data = &devs[dev_index];
+
+  analog_axis_set_raw_data_cb(data->dev, &analog_axis_raw_data_cb);
   while (true) {
-    shell_print(sh, "[%d, %d, %d]\t->\tx:%d, y:%d, z:%d.\tPress [S] to stop.", raw_val[0],
-                raw_val[1], raw_val[2], out_x, out_y, out_z);
+    for (size_t ch = 0; ch < data->num_channels; ch++) {
+      shell_fprintf(sh, SHELL_NORMAL,
+                    ch == 0                         ? "in: [ %d, "
+                    : ch < (data->num_channels - 1) ? "%d, "
+                                                    : "%d ]\t->\t",
+                    data->ch_data[ch].raw_val);
+    }
+
+    for (size_t ch = 0; ch < data->num_channels; ch++) {
+      shell_fprintf(sh, SHELL_NORMAL,
+                    ch == 0                         ? "out: [ %d, "
+                    : ch < (data->num_channels - 1) ? "%d, "
+                                                    : "%d ]\t\tPress [S] to stop.\n",
+                    data->ch_data[ch].out_val);
+    }
     if (stop(sh)) break;
-    k_msleep(300);
+    k_msleep(200);
   }
-  analog_axis_set_raw_data_cb(dev, NULL);
+  analog_axis_set_raw_data_cb(data->dev, NULL);
   return 0;
 }
 
 static int cmd_deadzone(const struct shell *sh, size_t argc, char **argv) {
+  struct device_data *data;
   struct analog_axis_calibration cal_data;
+  int dev_index;
 
-  if (check_device(sh)) return -1;
+  dev_index = atoi(argv[1]);
+  if (check_device(sh, dev_index)) return -1;
+  data = &devs[dev_index];
 
-  int num_axes = analog_axis_num_axes(dev);
   int ch = atoi(argv[1]);
-  if (ch >= num_axes || ch < 0) {
+  if (ch >= data->num_channels || ch < 0) {
     shell_print(sh, "Error: Invalid <ch> argument\n");
     return -1;
   }
 
-  analog_axis_calibration_get(dev, ch, &cal_data);
+  analog_axis_calibration_get(data->dev, ch, &cal_data);
   shell_print(sh, "Old setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, cal_data.in_min,
               cal_data.in_max, cal_data.in_deadzone);
 
   cal_data.in_deadzone = atoi(argv[2]);
 
-  analog_axis_calibration_set(dev, ch, &cal_data);
-  analog_axis_calibration_save(dev);
+  analog_axis_calibration_set(data->dev, ch, &cal_data);
+  analog_axis_calibration_save(data->dev);
 
-  analog_axis_calibration_get(dev, ch, &cal_data);
+  analog_axis_calibration_get(data->dev, ch, &cal_data);
   shell_print(sh, "New setting: ch.%d in_min: %d, in_max: %d, in_deadzone: %d", ch, cal_data.in_min,
               cal_data.in_max, cal_data.in_deadzone);
 
   return 0;
 }
 
+#define DEFINE_CALLBACK(i, node)                                  \
+  static void analog_axis_listener_##i(struct input_event *evt) { \
+    analog_axis_listener(DEVICE_DT_GET(node), i, evt);            \
+  }                                                               \
+                                                                  \
+  INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(node), analog_axis_listener_##i);
+
+FOR_EACH_IDX(DEFINE_CALLBACK, (), ANALOG_AXIS_NODE_LIST)
+
 SHELL_STATIC_SUBCMD_SET_CREATE(
   analog_axis_sub_cmds, SHELL_CMD_ARG(show, NULL, "Show current calibration data", cmd_show, 1, 0),
-  SHELL_CMD_ARG(cal, NULL, "Calibration\tUsage: analog_axis cal <ch> <out_min> <out_max>", cmd_cal,
+  SHELL_CMD_ARG(cal, NULL, "Calibration\tUsage: analog_axis cal <dev> <ch>", cmd_cal, 3, 0),
+  SHELL_CMD_ARG(test, NULL, "Test analog_axis\tUsage: analog_axis test <dev>", cmd_test, 2, 0),
+  SHELL_CMD_ARG(deadzone, NULL,
+                "Set deadzone value\tUsage: analog_axis deadzone <dev> <ch> <value>", cmd_deadzone,
                 4, 0),
-  SHELL_CMD_ARG(test, NULL, "Test analog_axis\tUsage: analog_axis test, cmd_test", cmd_test, 1, 0),
-  SHELL_CMD_ARG(deadzone, NULL, "Set deadzone value\tUsage: analog_axis deadzone <ch> <value>",
-                cmd_deadzone, 3, 0),
 
   SHELL_SUBCMD_SET_END /* Array terminated. */
 );
